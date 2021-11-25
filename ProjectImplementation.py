b357ee4eca4f09baf37a5281f1134525fcedbdef
jarlo72 <jmedelicano@gmail.com>
2021-11-25 13:28:57 -0500

    implementation start

---
+++ ProjectImplementation.py
@@ -0,0 +1,27 @@
+# 1. PROGRAM STATEMENTS ###################################
+
+
+# Structure of implementation follows the algorithm
+
+# 2. FUNCTION DEFINITIONS AND IMPORTS ##################################
+
+
+# 3. MAIN FUNCTION ##################################
+
+# def main():
+
+## 3.1 INPUT LOOP ###################################
+
+## 3.2 MAIN PROCESSING ###################################
+
+# i. Angle of Misclosure and Balancing -----------------
+
+# ii. Bearings and Azimuths ------------------ 
+
+# iii. Latitudes and Longitudes -----------------------
+
+# iv. Draw traverse with Turtle graphics ------------------
+
+# v. Error of Closure and Precision Ratio --------------------
+
+## 3.3  OUTPUT LOOP ###################################
