fb5a493fe2668112588a048ab15d1ec52bbe9077
jarlo72 <jmedelicano@gmail.com>
2021-11-25 13:31:26 -0500

    implementation_structure_documentation

--- ProjectImplementation.py
+++ ProjectImplementation.py
@@ -1,5 +1,16 @@
-# 1. PROGRAM STATEMENTS ###################################
+# 1. PROGRAM METADATA ###################################
 
+# Calc_Cave_Depth3.0.py
+# Created by: Class 2021
+# Edited by: Jon Marlo Delicano 
+# For: Assignment 2 Deliverable 2
+# Date: November 24, 2021
+# Program function: Calculates the depth below the surface using radiolocation
+# Farrant, Andrew & Mullan, Graham. (2018). Novel use of radio-location for a ground investigation at Pen Park Hole, Bristol, United Kingdom
+# Uses the distance the receiver is from ground zero and angle of the receiver 
+# Assumes the angle of the receiver is in degrees and that the coordinates for Ground Zero and the Receiver's location is referencing a cartesian plane using metric(meter) units.
+# Inputs entered with keyboard
+# Outputs displayed on screen
 
 # Structure of implementation follows the algorithm
 
@@ -8,11 +19,11 @@
 
 # 3. MAIN FUNCTION ##################################
 
-# def main():
+# 3.0 PROGRAM STATEMENTS
 
-## 3.1 INPUT LOOP ###################################
+# 3.1 INPUT LOOP 
 
-## 3.2 MAIN PROCESSING ###################################
+# 3.2 MAIN PROCESSING
 
 # i. Angle of Misclosure and Balancing -----------------
 
@@ -24,4 +35,4 @@
 
 # v. Error of Closure and Precision Ratio --------------------
 
-## 3.3  OUTPUT LOOP ###################################
+# 3.3  OUTPUT LOOP 
